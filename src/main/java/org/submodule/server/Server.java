d5819116bf7708ef79f0252af73d49e55f979f62
Nico Abrasaldo <nicoiabrasaldo@yahoo.com>
2022-07-13 23:19:50 +0800

    Modified server class

--- src/main/java/org/submodule/server/Server.java
+++ src/main/java/org/submodule/server/Server.java
@@ -2,6 +2,6 @@ package org.submodule.server;
 
 public class Server {
     public static void main(String[] args) {
-        System.out.println("Hello from Server!");
+        System.out.println("Hello from Server!!!");
     }
 }
